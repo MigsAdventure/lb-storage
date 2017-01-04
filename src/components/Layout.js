6b39ee116b37dd49fc4f8ade8c2b1fcf441707b4
Miguel Pardo <migsub77@gmail.com>
2017-01-03 16:41:38 -0800

    settup for new project

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from  'react-redux';
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text-center'>Test</h1>
+      </div>
+    )
+  }
+}
